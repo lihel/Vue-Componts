3cfff1554865ba83416595b56341fbadec92bbf7
lihel <github.com>
2020-01-20 17:43:25 +0800

    fix: 删除冗余代码

--- src/main.js
+++ src/main.js
@@ -4,11 +4,9 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 
-import VChart from 'v-charts'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
-Vue.use(VChart)
 
 new Vue({
   router,
